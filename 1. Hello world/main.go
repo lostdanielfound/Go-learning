89a77b020e7ac815ea4bc30e3bd243b46dfc674f
Daniel Guzman <danielsonguzman2000@gmail.com>
2023-02-26 23:24:20 -0800

    Hello world program

---
+++ 1. Hello world/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("This is my first program!")
+	fmt.Println("Hello World!")
+	fmt.Println("This function takes in a string literal and automatically adds a newline at the end")
+	fmt.Println("This also doesn't need a semicolign at the end of each statement")
+	fmt.Println("The time is: ", time.Now())
+}
